95f3a3ab102b83d1d06ab864777ca7d3e397cc6a
Melvin Low <la0bing07148@gmail.com>
2022-08-21 21:41:40 +0800

    Commented JIRA ticket required in commit message.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     ignores: [(message) => message.startsWith('Initial commit')],
-    parserPreset: {
-      parserOpts: {
-        issuePrefixes: ['[A-Z]{1,8}-[0-9]{1,4}'],
-        referenceActions: null,
-      },
-    },
+    // parserPreset: {
+    // parserOpts: {
+    //    issuePrefixes: ['[A-Z]{1,8}-[0-9]{1,4}'],
+    //    referenceActions: null,
+    //  },
+    //},
     rules: {
       'footer-max-line-length': [0],
       'references-empty': [2, 'never'],
